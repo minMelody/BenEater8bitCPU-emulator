51bd8ab49ffdc511c7c2252f22499b5b0d164879
minMelody <138899007+minMelody@users.noreply.github.com>
2023-09-02 19:36:28 +0200

    13th video

--- BE8bitCPU.cpp
+++ BE8bitCPU.cpp
@@ -3,12 +3,17 @@
 // Building an 8-bit breadboard computer! - Ben Eater
 // https://youtube.com/playlist?list=PLowKtXNTBypGqImE405J2565dvjafglHU&feature=shared
 
-struct CPU {
+typedef unsigned char Byte;
 
+struct CPU {
+	// registers
+	Byte A, B;
+	Byte Ins;	// instruction register: bits 0xF0 -> instruction, bits 0x0F -> data
 };
 
 int main()
 {
 	CPU cpu;
+
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# BenEater8bitCPU-emulator
- Emulating Ben Eater's 8 bit CPU
+# DESCRPTION
+Emulting Ben Eater's 8 bit computer
+
+[Building an 8-bit breadboard computer! - Ben Eater](https://youtube.com/playlist?list=PLowKtXNTBypGqImE405J2565dvjafglHU&feature=shared)
