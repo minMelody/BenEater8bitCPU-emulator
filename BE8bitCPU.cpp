ed10e61c3870bd387a60b89527d2462d52c0e155
minMelody <138899007+minMelody@users.noreply.github.com>
2023-09-02 16:36:48 +0200

    Initial commit

---
+++ BE8bitCPU.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Building an 8-bit breadboard computer! - Ben Eater
+// https://youtube.com/playlist?list=PLowKtXNTBypGqImE405J2565dvjafglHU&feature=shared
+
+struct CPU {
+
+};
+
+int main()
+{
+	CPU cpu;
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BenEater8bitCPU-emulator
+ Emulating Ben Eater's 8 bit CPU
